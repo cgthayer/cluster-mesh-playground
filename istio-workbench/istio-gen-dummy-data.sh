f624a90c46ee0aa2f6f21cd18e39b301114c3d29
Charles Thayer <thayer@b2si.com>
2023-01-28 16:52:44 -0800

    better ready checking

--- istio-workbench/istio-gen-dummy-data.sh
+++ istio-workbench/istio-gen-dummy-data.sh
@@ -4,7 +4,7 @@ POD=$(kubectl get pod -l app=ratings -o jsonpath='{.items[0].metadata.name}')
 HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.clusterIP}')
 
 max=30
-echo -n "Sending ${max} requests: "
+echo -n "Sending ${max} requests to pod=$POD host=$HOST: "
 for i in $(seq 1 $max); do
     echo -n "$i "
     kubectl exec ${POD} -- curl -s -o /dev/null ${HOST}/productpage
